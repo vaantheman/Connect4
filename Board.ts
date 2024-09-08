8e47f326fcb63d05ca84e9c7065d6e011e85ffca
Van Loc Doan <vaan.l.doan@gmail.com>
2024-09-08 21:25:17 +0200

    move logic

--- Board.ts
+++ Board.ts
@@ -14,6 +14,28 @@ export class Board {
         for (let row of this.grid) {
             console.log(row.map((cell) => (cell ? cell : '.')).join(' '));
         }
-        console.log('1 2 3 4 5 6 7');
+        console.log('0 1 2 3 4 5 6');
+    }
+
+    isValidMove(col: number): boolean {
+        return col >= 0 && col < this.cols && this.grid[0][col] === null;
+    }
+
+    makeMove(col: number, symbol: Cell): boolean {
+        if (!this.isValidMove(col)) return false;
+
+        for (let row = this.rows - 1; row >= 0; row--) {
+            if (this.grid[row][col] === null) {
+                this.grid[row][col] = symbol;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    getValidMoves(): number[] {
+        return this.grid[0]
+            .map((cell, col) => (cell === null ? col : -1))
+            .filter((col) => col !== -1);
     }
 }
