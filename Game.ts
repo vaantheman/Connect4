de1d22538c83b3b0180e3444d4c8fdd2d192b3f9
Van Loc Doan <vaan.l.doan@gmail.com>
2024-09-08 21:37:42 +0200

    get player move and use readline for input

--- Game.ts
+++ Game.ts
@@ -4,9 +4,60 @@ import { Player } from './Player';
 export class Game {
     private board: Board;
     private players: [Player, Player];
+    private currentPlayerIndex: number = 0;
 
     constructor(player1: Player, player2: Player) {
         this.board = new Board();
         this.players = [player1, player2];
+    }
+
+    start(): void {
+        let winner: Player | null = null;
+
+        while (true) {
+            this.board.render();
+            let currentPlayer = this.players[this.currentPlayerIndex];
+
+            let col: number;
+            if (currentPlayer.type === 'human') {
+                col = this.promptMove(currentPlayer);
+            } else {
+                col = currentPlayer.getMove(this.board);
+                console.log(`${currentPlayer.name} (Computer) chose column ${col}`);
+            }
+
+            if (this.board.makeMove(col, currentPlayer.symbol)) {
+                if (this.board.checkWin(currentPlayer.symbol)) {
+                    winner = currentPlayer;
+                    break;
+                }
+
+                if (this.board.isFull()) {
+                    break;
+                }
+
+                this.currentPlayerIndex = 1 - this.currentPlayerIndex;
+            } else {
+                console.log('Invalid move. Try again.');
+            }
+        }
+
+        this.board.render();
+        if (winner) {
+            console.log(`${winner.name} wins!`);
+        } else {
+            console.log("It's a tie!");
+        }
+    }
+
+    promptMove(player: Player): number {
+        const readlineSync = require('readline-sync');
+        let col: number;
+        do {
+            col = readlineSync.questionInt(
+                `${player.name} (${player.symbol}), choose a column (0-6): `
+            );
+        } while (!this.board.isValidMove(col));
+        return col;
     }
 }
--- Player.ts
+++ Player.ts
@@ -3,4 +3,12 @@ import { Cell, PlayerType } from './types';
 
 export class Player {
     constructor(public name: string, public type: PlayerType, public symbol: Cell) {}
+
+    getMove(board: Board): number {
+        if (this.type === 'computer') {
+            let validMoves = board.getValidMoves();
+            return validMoves[Math.floor(Math.random() * validMoves.length)];
+        }
+        return -1;
+    }
 }
